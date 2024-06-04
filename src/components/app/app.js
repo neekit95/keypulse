b679078268732d985c1f628e898849e741fb02ef
neekit95 <nikita-ribalkin@mail.ru>
2024-06-04 12:50:43 +0300

    create keyboard simulator

---
+++ src/components/app/app.js
@@ -0,0 +1,38 @@
+import style from './app.module.scss';
+import Header from "../header/header";
+import {RiSpeedUpLine} from "react-icons/ri";
+
+const App = () => {
+	
+	const arr = () => {
+	
+	}
+	return (
+		<div className={style.container}>
+			
+			<Header/>
+			
+			<div className="wrapper">
+				<div className={style.main}>
+					<div className={style.theGame}>
+						game
+					</div>
+					
+					<div className={style.options}>
+						<div className={style.option}>
+							<RiSpeedUpLine />
+							<p className={style.p}>Speed</p>
+						</div>
+						
+						<div className={style.option}>
+							Accuracy
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
+
---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './header.module.scss';
+
+const Header = () => {
+	return (
+		<div className={style.container}>
+			Keyboard simulator
+		</div>
+	);
+};
+
+export default Header;
+
