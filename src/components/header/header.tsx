731c19284720bdd299f648f8601a912222bce9b7
neekit95 <nikita-ribalkin@mail.ru>
2024-08-06 20:57:07 +0300

    add icons, fonts

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import style from './header.module.scss';
 import {VscAccount} from "react-icons/vsc";
+import {BsKeyboard} from "react-icons/bs";
 import {Link} from 'react-router-dom';
+
 
 const Header = () => {
     return (
@@ -10,15 +12,16 @@ const Header = () => {
                 <div className={style.links}>
                     <Link to='/'>
                         <button>
-                            Keyboard simulator
+                            <BsKeyboard className={style.icon}/>
+                            KeyPulse
                         </button>
                     </Link>
                 </div>
                 <div className={style.links}>
                     <Link to='/user'>
-                        <button className={style.profileButton}>
-                            <VscAccount/>
-                            <p> My Profile </p>
+                        <button>
+                            <VscAccount className={style.icon}/>
+                            My Profile
                         </button>
                     </Link>
                 </div>
