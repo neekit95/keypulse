014e11fda6085b7fb50dd3deeb911d4309d20745
neekit95 <nikita-ribalkin@mail.ru>
2024-08-06 18:52:08 +0300

    header styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,21 +6,24 @@ import {Link} from 'react-router-dom';
 const Header = () => {
     return (
         <div className={style.container}>
-            <div className={style.name}>
-                <Link to='/'>
-                    <button>
-                        Keyboard simulator
-                    </button>
-                </Link>
+            <div className={style.wrapper}>
+                <div className={style.links}>
+                    <Link to='/'>
+                        <button>
+                            Keyboard simulator
+                        </button>
+                    </Link>
+                </div>
+                <div className={style.links}>
+                    <Link to='/user'>
+                        <button className={style.profileButton}>
+                            <VscAccount/>
+                            <p> My Profile </p>
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <div className={style.profile}>
-                <Link to='/user'>
-                    <button className={style.profileButton}>
-                        <VscAccount/>
-                        <p> My Profile </p>
-                    </button>
-                </Link>
-            </div>
+
         </div>
     );
 };
